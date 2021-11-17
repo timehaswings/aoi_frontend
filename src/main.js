48532758ec288ae7a99e4a3bb936ddee883c07aa
王刚 <wanggang6595@cvte.com>
2021-11-17 11:17:01 +0800

    [feature] add postcss, add env config

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import api from './axios'
-import installElementPlus from './plugins/element'
-import './assets/css/app.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import api from './axios';
+import installElementPlus from './plugins/element';
+import animate from 'animate.css';
+import 'normalize.css/normalize.css';
+import './assets/css/app.css';
+// import 'animate.css/animate.min.css';
 
-
-const app = createApp(App)
+const app = createApp(App);
 app.config.globalProperties.$api = api;
-installElementPlus(app)
+installElementPlus(app);
 
-app.use(router).use(store).mount('#app')
+app.use(router).use(store).use(animate).mount('#app');
--- src/views/Home/About.vue
+++ src/views/Home/About.vue
@@ -1,16 +1,18 @@
 <template>
-    <div>
-       关于我们
-    </div>
+  <div class="animate__animated animate__bounceInLeft">
+    <span>dsds</span>
+  </div>
 </template>
 
 <script>
-
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
+.router_animate-enter-active {
+  animation: rollIn 1s;
+}
+.router_animate-leave-active {
+  animation: rollOut 0.6s;
+}
 </style>
