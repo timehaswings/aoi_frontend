4a7e349fbc40acdf21b00a42131073f1f8462c6c
王刚 <wanggang6595@cvte.com>
2021-11-22 10:42:10 +0800

    [feature] add login.vue

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import router from './router';
 import store from './store';
 import api from './axios';
 import installElementPlus from './plugins/element';
-import animate from 'animate.css';
 import 'normalize.css/normalize.css';
+import 'animate.css';
 import './assets/css/app.css';
-// import 'animate.css/animate.min.css';
 
 const app = createApp(App);
 app.config.globalProperties.$api = api;
 installElementPlus(app);
 
-app.use(router).use(store).use(animate).mount('#app');
+app.use(router).use(store).mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,106 +1,116 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
-NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
+NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false });
 
 const routes = [
-    {
-        path: '/',
-        name: 'Index',
-        meta: {
-            title: '主页'
-        },
-        component: () => import('../views/Index.vue'),
-        children: [
-            {
-                path: '',
-                name: 'HomeIndex',
-                meta: {
-                    title: 'HomeIndex'
-                },
-                component: () => import('../views/Home/Index.vue')
-            },
-            {
-                path: '/category',
-                name: 'HomeCategory',
-                meta: {
-                    title: 'HomeCategory'
-                },
-                component: () => import('../views/Home/Category.vue')
-            },
-            {
-                path: '/member',
-                name: 'HomeMember',
-                meta: {
-                    title: 'HomeMember'
-                },
-                component: () => import('../views/Home/Member.vue')
-            },
-            {
-                path: '/personal',
-                name: 'HomePersonal',
-                meta: {
-                    title: 'HomePersonal'
-                },
-                component: () => import('../views/Home/Personal.vue')
-            },
-            {
-                path: '/about',
-                name: 'HomeAbout',
-                meta: {
-                    title: 'HomeAbout'
-                },
-                component: () => import('../views/Home/About.vue')
-            }
-        ]
-    }, {
-        path: '/backend',
-        name: 'Backend',
-        meta: {
-            title: '后台管理'
-        },
-        component: () => import ('../views/Backend/Index.vue'),
-        children: [
-            {
-                path: '',
-                name: 'BackendSummary',
-                meta: {
-                    title: 'BackendSummary'
-                },
-                component: () => import('../views/Backend/Summary.vue')
-            },
-        ]
+  {
+    path: '/',
+    name: 'Index',
+    meta: {
+      title: '主页',
     },
-    {
-        path: '/404',
-        name: '404',
+    component: () => import('../views/Index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'HomeIndex',
         meta: {
-            title: '找不到页面'
+          title: 'HomeIndex',
         },
-        component: () => import ('../views/404.vue')
-    }, {
-        path: '/403',
-        name: '403',
+        component: () => import('../views/Home/Index.vue'),
+      },
+      {
+        path: '/category',
+        name: 'HomeCategory',
         meta: {
-            title: '没有权限'
+          title: 'HomeCategory',
         },
-        component: () => import ('../views/403.vue')
-    }
+        component: () => import('../views/Home/Category.vue'),
+      },
+      {
+        path: '/member',
+        name: 'HomeMember',
+        meta: {
+          title: 'HomeMember',
+        },
+        component: () => import('../views/Home/Member.vue'),
+      },
+      {
+        path: '/personal',
+        name: 'HomePersonal',
+        meta: {
+          title: 'HomePersonal',
+        },
+        component: () => import('../views/Home/Personal.vue'),
+      },
+      {
+        path: '/about',
+        name: 'HomeAbout',
+        meta: {
+          title: 'HomeAbout',
+        },
+        component: () => import('../views/Home/About.vue'),
+      },
+    ],
+  },
+  {
+    path: '/backend',
+    name: 'Backend',
+    meta: {
+      title: '后台管理',
+    },
+    component: () => import('../views/Backend/Index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'BackendSummary',
+        meta: {
+          title: 'BackendSummary',
+        },
+        component: () => import('../views/Backend/Summary.vue'),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {
+      title: '找不到页面',
+    },
+    component: () => import('../views/Home/Login.vue'),
+  },
+  {
+    path: '/404',
+    name: '404',
+    meta: {
+      title: '找不到页面',
+    },
+    component: () => import('../views/404.vue'),
+  },
+  {
+    path: '/403',
+    name: '403',
+    meta: {
+      title: '没有权限',
+    },
+    component: () => import('../views/403.vue'),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
-    NProgress.start();
-    next();
+  NProgress.start();
+  next();
 });
 
 router.afterEach(() => {
-    NProgress.done()
+  NProgress.done();
 });
 
 export default router;
