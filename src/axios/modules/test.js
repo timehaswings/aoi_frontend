6a1c82b7128d5d07a164f2637e181525e5c5d67c
“lnowordss <timehaswingss@gmail.com>
2021-12-13 21:28:05 +0800

    [maintain] improve axios lib

--- src/axios/modules/test.js
+++ src/axios/modules/test.js
@@ -1,13 +1,13 @@
-import axios from "../axios"
+import request from "../axios"
 
 /**
  * 测试接口
  * @param {*} data 
  */
-export const test = params => {
-  return axios.request({
-    url: "/api/v1/test",
-    method: "get",
-    params
-  })
-}
+export const test = params => request({
+  url: "/api/v1/test",
+  method: "get",
+  params
+})
+
+
